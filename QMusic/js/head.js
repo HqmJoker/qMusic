cbc9591bfdb82de0f307efd29264ad502497c97d
unknown <18344064827@163.com>
2020-12-16 15:06:19 +0800

    增加暗黑模式

--- QMusic/js/head.js
+++ QMusic/js/head.js
@@ -4,6 +4,22 @@ $.ajax({
     success:function(header){
         $(header).replaceAll("header");
         $("<link rel='stylesheet' href='css/head.css'/>").appendTo("header");
+        $('.change-theme').click(function(){
+            let toggle_text = $(this).find('.toggle-text');
+            if( toggle_text.text() === 'on') {
+                $(this).removeClass('theme-activted');
+                toggle_text.text('off');
+                $("html").css({ "filter": "invert(0) hue-rotate(0deg)" });
+                $("html").find('img').css({ "filter": "invert(0) hue-rotate(0deg)" });
+                $("html").find('.logo').css({ "filter": "invert(0) hue-rotate(0deg)" });
+            }else{
+                $(this).addClass('theme-activted');
+                toggle_text.text('on');
+                $("html").css({ "transition": "all 300ms", "filter": "invert(1) hue-rotate(180deg)" });
+                $("html").find('img').css({ "filter": "invert(1) hue-rotate(180deg)" });
+                $(".logo").css({ "filter": "invert(0) hue-rotate(0deg)"});
+            }
+        });
         $.ajax({
             url:"http://127.0.0.1:3000/show",
             type:"get",
